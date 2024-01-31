c062bddc08ea8b8acbef0c47c0870f07ce6d1ae0
Francisco Castanheira <franciscocastanheira1998@gmail.com>
2024-01-31 23:34:37 +0100

    Starting to read and validate XML

--- mkm_bot/main.py
+++ mkm_bot/main.py
@@ -1,5 +1,31 @@
+import sys
+import lxml
+import __main__
+
+from pathlib import Path
+from lxml import etree
+
+
+def read_and_validate_xml_config(xml_path: Path) -> etree:
+    lxml.etree.parse(xml_path)
+
+    # assert hasattr(__main__, __file__)  # need to figure out how to do this properly
+    xsd_path = Path(__main__.__file__)
+    xsd: lxml.etree
+    with open(xsd_path, 'r') as fp:
+        xsd = etree.XMLSchema(etree.parse(fp))
+    with open(xml_path, 'r') as fp:
+        xml = lxml.etree.parse(fp)
+
+    xsd.assertValid(xml)
+
+
 def main() -> None:
+    # assert sys.argc == 2
+    print(__main__.__file__)
+    xml_root = read_and_validate_xml_config(sys.argv[1])
     print('Hello, World!')
+    print(sys.argv[0])
     return
 
 
